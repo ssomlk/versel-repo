f8bd042c527ccf7a5a65c6a00761b478a1a3ba51
ssomlk <shankasomasiri@gmail.com>
2021-10-09 22:59:29 +0530

    test branch protection rue

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
       <Head>
         <title>iit</title>
       </Head>
-      
+      {/* Test block */}
       <h1 className='h1'>Welcome to International Institute of Theravada</h1>
     </div>
   );
