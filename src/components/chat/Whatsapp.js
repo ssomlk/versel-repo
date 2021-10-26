3a31613d71d70a05fc25ea92ddfd09c71deffdbe
ssomlk <shankasomasiri@gmail.com>
2021-10-26 10:14:09 +0530

    add styles to whatsapp component

--- src/components/chat/Whatsapp.js
+++ src/components/chat/Whatsapp.js
@@ -4,7 +4,11 @@ import WhatsAppWidget from 'react-whatsapp-widget';
 import 'react-whatsapp-widget/dist/index.css';
 
 const Whatsapp = () => {
-  return <WhatsAppWidget phoneNumber='+94778374845' companyName="International Institute of Theravada"/>
+  return(
+    <div className="whatsapp">
+        <WhatsAppWidget phoneNumber='+94778374845' companyName="International Institute of Theravada"/>
+    </div>
+  );
 }
 
 export default Whatsapp;
